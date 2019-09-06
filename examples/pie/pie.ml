cce2e69d63f747aabf8ee483badcc0032c4295a8
monstasat <sashayanin@gmail.com>
2019-09-06 12:40:11 +0300

    examples fixes

--- examples/pie/pie.ml
+++ examples/pie/pie.ml
@@ -1,49 +1,41 @@
 open Js_of_ocaml
-open Chartjs
 
 let () =
   let border_color_fun context =
-    Color.of_string
-    @@
-    match context##.dataIndex with
-    | 0 -> "red"
-    | 1 -> "blue"
-    | 2 -> "green"
-    | _ -> "initial)"
+    Chartjs.Color.of_string
+      (match context##.dataIndex with
+      | 0 -> "red"
+      | 1 -> "blue"
+      | 2 -> "green"
+      | _ -> "initial")
   in
   let background_color_fun context =
-    Color.of_string
-    @@
-    match context##.dataIndex with
-    | 0 -> "pink"
-    | 1 -> "lightblue"
-    | 2 -> "lightgreen"
-    | _ -> "initial)"
+    Chartjs.Color.of_string
+      (match context##.dataIndex with
+      | 0 -> "pink"
+      | 1 -> "lightblue"
+      | 2 -> "lightgreen"
+      | _ -> "initial")
   in
   (* Create dataset. *)
-  let dataset = create_pie_dataset () in
+  let dataset = Chartjs.empty_pie_dataset () in
   dataset##.data := Js.array [|40; 15; 20|];
-  dataset##.borderColor := Scriptable_indexable.of_fun border_color_fun;
-  dataset##.backgroundColor := Scriptable_indexable.of_fun background_color_fun;
-  dataset##.borderWidth := Scriptable_indexable.of_single 5;
+  dataset##.borderColor := Chartjs.Scriptable_indexable.of_fun border_color_fun;
+  dataset##.backgroundColor := Chartjs.Scriptable_indexable.of_fun background_color_fun;
+  dataset##.borderWidth := Chartjs.Scriptable_indexable.of_single 5;
   dataset##.label := Js.string "Dataset 1";
   (* Create chart data. *)
-  let data = create_data () in
+  let data = Chartjs.empty_data () in
   data##.datasets := Js.array [|dataset|];
   data##.labels := Js.array @@ Array.map Js.string [|"first"; "second"; "third"|];
-  let legend = create_legend () in
-  let animation = create_pie_animation () in
-  let options = create_pie_options () in
+  let legend = Chartjs.empty_legend () in
+  let animation = Chartjs.empty_pie_animation () in
+  let options = Chartjs.empty_pie_options () in
   animation##.animateScale := Js._true;
   animation##.animateRotate := Js._true;
-  legend##.position := Position.left;
-  legend##.onHover :=
-    Js.wrap_meth_callback (fun _ _ item ->
-        let log x : unit = Js.Unsafe.global##.console##log x in
-        log item;
-        print_endline "hover legend");
+  legend##.position := Chartjs.Position.left;
   options##.cutoutPercentage := 20.;
   options##.animation := animation;
   options##.legend := legend;
-  let pie = chart_from_id Chart.doughnut data options "chart" in
+  let pie = Chartjs.chart_from_id Chartjs.Chart.pie data options "chart" in
   Js.Unsafe.global##.chart := pie
--- examples/timescale/timescale.ml
+++ examples/timescale/timescale.ml
@@ -1,38 +1,38 @@
 open Js_of_ocaml
-open Chartjs
 
 let format = "MM/DD/YYYY HH:mm"
 
-let new_date year = Time.of_array [|year|]
+let new_date year = Chartjs.Time.of_array [|year|]
 
 let random () = Random.int 10
 
 let () =
   Random.init (Float.to_int (new%js Js.date_now)##getTime);
-  let dataset1 = create_line_dataset () in
+  let dataset1 = Chartjs.empty_line_dataset () in
   dataset1##.data := Js.array @@ Array.init 7 (fun _ -> random ());
   dataset1##.label := Js.string "My First Dataset";
-  dataset1##.backgroundColor := Color.of_string "rgba(255, 0, 0, 0.5)";
-  dataset1##.borderColor := Color.of_string "red";
-  dataset1##.fill := Line_fill._false;
-  let dataset2 = create_line_dataset () in
+  dataset1##.backgroundColor := Chartjs.Color.of_string "rgba(255, 0, 0, 0.5)";
+  dataset1##.borderColor := Chartjs.Color.of_string "red";
+  dataset1##.fill := Chartjs.Line_fill._false;
+  let dataset2 = Chartjs.empty_line_dataset () in
   dataset2##.data := Js.array @@ Array.init 7 (fun _ -> random ());
   dataset2##.label := Js.string "My Second Dataset";
-  dataset2##.backgroundColor := Color.of_string "rgba(0, 0, 255, 0.5)";
-  dataset2##.borderColor := Color.of_string "blue";
-  dataset2##.fill := Line_fill._false;
-  let dataset3 = create_line_dataset () in
+  dataset2##.backgroundColor := Chartjs.Color.of_string "rgba(0, 0, 255, 0.5)";
+  dataset2##.borderColor := Chartjs.Color.of_string "blue";
+  dataset2##.fill := Chartjs.Line_fill._false;
+  let dataset3 = Chartjs.empty_line_dataset () in
   dataset3##.data :=
     Js.array
-    @@ [| create_data_point ~x:(new_date 1990) ~y:(random ())
-        ; create_data_point ~x:(new_date 1992) ~y:(random ())
-        ; create_data_point ~x:(new_date 1994) ~y:(random ())
-        ; create_data_point ~x:(new_date 1996) ~y:(random ()) |];
+      Chartjs.
+        [| create_data_point ~x:(new_date 1990) ~y:(random ())
+         ; create_data_point ~x:(new_date 1992) ~y:(random ())
+         ; create_data_point ~x:(new_date 1994) ~y:(random ())
+         ; create_data_point ~x:(new_date 1996) ~y:(random ()) |];
   dataset3##.label := Js.string "Dataset with point data";
-  dataset3##.backgroundColor := Color.of_string "rgba(0, 255, 0, 0.5)";
-  dataset3##.borderColor := Color.of_string "green";
-  dataset3##.fill := Line_fill._false;
-  let data = create_data () in
+  dataset3##.backgroundColor := Chartjs.Color.of_string "rgba(0, 255, 0, 0.5)";
+  dataset3##.borderColor := Chartjs.Color.of_string "green";
+  dataset3##.fill := Chartjs.Line_fill._false;
+  let data = Chartjs.empty_data () in
   data##.labels :=
     Js.array
       [| new_date 1990
@@ -43,33 +43,35 @@ let () =
        ; new_date 1995
        ; new_date 1996 |];
   data##.datasets :=
-    Js.array [|coerce_dataset dataset1; coerce_dataset dataset2; coerce_dataset dataset3|];
+    Js.array
+      Chartjs.
+        [|coerce_dataset dataset1; coerce_dataset dataset2; coerce_dataset dataset3|];
   (* Initialize title *)
-  let title = create_title () in
+  let title = Chartjs.empty_title () in
   title##.display := Js._true;
-  title##.text := Indexable.of_single @@ Js.string "Chart.js Time Scale";
+  title##.text := Chartjs.Indexable.of_single @@ Js.string "Chart.js Time Scale";
   (* Initialize scales *)
-  let time = create_time_cartesian_options () in
-  let (scaleLabel : scaleLabel Js.t) = Js.Unsafe.obj [||] in
-  let xAxis = create_time_cartesian_axis () in
-  time##._parser := Time_parser.of_string format;
+  let time = Chartjs.empty_time_options () in
+  let scaleLabel = Chartjs.empty_scale_label () in
+  let xAxis = Chartjs.empty_time_axis () in
+  time##._parser := Chartjs.Time_parser.of_string format;
   time##.tooltipFormat := Js.string "ll HH:mm";
   scaleLabel##.display := Js._true;
   scaleLabel##.labelString := Js.string "Date";
   xAxis##.time := time;
   xAxis##.scaleLabel := scaleLabel;
-  let yAxis = create_cartesian_axis () in
-  let (scaleLabel : scaleLabel Js.t) = Js.Unsafe.obj [||] in
+  let yAxis = Chartjs.empty_cartesian_axis () in
+  let scaleLabel = Chartjs.empty_scale_label () in
   scaleLabel##.display := Js._true;
   scaleLabel##.labelString := Js.string "value";
   yAxis##.scaleLabel := scaleLabel;
-  let scales = create_line_scales () in
+  let scales = Chartjs.empty_line_scales () in
   scales##.xAxes := Js.array [|xAxis|];
   scales##.yAxes := Js.array [|yAxis|];
   (* Initialize other options *)
-  let options = create_line_options () in
+  let options = Chartjs.empty_line_options () in
   options##.scales := scales;
   options##.title := title;
   options##.maintainAspectRatio := Js._false;
-  let chart = chart_from_id Chart.line data options "chart" in
+  let chart = Chartjs.chart_from_id Chartjs.Chart.line data options "chart" in
   Js.Unsafe.global##.chart := chart
